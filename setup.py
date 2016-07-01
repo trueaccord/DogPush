74342704342d5b6b344db3062981ac364139506e
Nadav Samet <thesamet@gmail.com>
2016-07-01 08:48:43 -0700

    bump to 0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='DogPush',
-      version='0.3.2',
+      version='0.3.3',
       description='DogPush: manage datadog alerts in local files.',
       author='Nadav S Samet',
       author_email='thesamet@gmail.com',
